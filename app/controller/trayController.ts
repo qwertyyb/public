712b0afbde1c085615873344e93ea30bec02e1c2
qwertyyb <qwertyyb@qq.com>
2021-03-01 00:02:04 +0800

    feat: fix some bug

---
+++ app/controller/trayController.ts
@@ -0,0 +1,37 @@
+import {
+  Tray as ElectronTray,
+  Menu as ElectronMenu,
+  nativeImage as ElectronNativeImage,
+} from 'electron'
+import { CoreApp } from 'index';
+
+let publicTray: ElectronTray | undefined
+
+
+module.exports = (publicApp: CoreApp) => ({
+  getTray() {
+    return publicTray;
+  },
+  createTray() {
+    if (publicTray) return publicTray
+    const path = require('path')
+    const { nativeImage, Menu , Tray } : typeof Electron = require('electron')
+    const image = nativeImage.createFromPath(path.join(__dirname, '../../assets/status/status.png'))
+    publicTray = new Tray(image)
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: '显示',
+        click: () => {
+          publicApp.window.main?.show()
+          publicApp.electronApp.focus({
+            steal: true
+          })
+        }
+      },
+      { label: '关于', role: 'about' },
+      { label: '退出', role: 'quit' }
+    ])
+    publicTray?.setToolTip('Public Tools')
+    publicTray?.setContextMenu(contextMenu)
+  }
+})
