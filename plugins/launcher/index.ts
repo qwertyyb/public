4337a4ffd2949cc0f03a40cc56cd165de4d4ab74
qwertyyb <qwertyyb@qq.com>
2021-03-01 22:56:31 +0800

    fix: 优化获取应用的速度,使用fork子进程,避免应用卡死

--- plugins/launcher/index.ts
+++ plugins/launcher/index.ts
@@ -1,46 +1,32 @@
-import fs from 'fs'
+// import fs from 'fs'
 import * as path from 'path'
+import { fork } from 'child_process'
 // @ts-ignore
-import fileIcon from 'file-icon'
-import mdfind from './mdfind'
+// import fileIcon from 'file-icon'
+// import mdfind from './mdfind'
 import { CommonListItem, PublicPlugin } from 'shared/types/plugin'
 
-interface App {
+interface AppListItem extends CommonListItem{
   name: string,
   path: string,
   icon: string,
 }
 
-const getApplicationSupportPath = () => {
-  const path = require('path')
-  const subPath = '/cn.qwertyyb.public/launcher'
-  const fullPath = path.join(process.env.HOME + '/Library/Application Support/', subPath);
-  return fullPath
-}
-
-const macosAppPaths = [
-  '/System/Applications', // 系统应用
-  '/Applications',  // 安装的应用
-]
-
-
-/**
- * List of supported files
- * @type {Array}
- */
-const supportedTypes = [
-  'com.apple.application-bundle',
-  'com.apple.systempreference.prefpane'
-]
-
-/**
- * Build mdfind query
- *
- * @return {String}
- */
-const buildQuery = () => (
-  supportedTypes.map(type => `kMDItemContentType=${type}`).join('||')
-)
+const getAppList = (() => {
+  let childProcess: ReturnType<typeof fork> | undefined
+  return () => {
+    childProcess?.kill()
+    return new Promise((resolve) => {
+      childProcess = fork(
+        path.resolve(__dirname, './loadApplications')
+      )
+      childProcess.on('message', (message) => {
+        console.log(message.length)
+        resolve(message)
+      })
+    })
+  }
+})()
 
 class LauncherPlugin implements PublicPlugin {
 
@@ -58,42 +44,10 @@ class LauncherPlugin implements PublicPlugin {
     })
   }
 
-  private apps: CommonListItem[] = []
+  private apps: AppListItem[] = []
 
   private getAppList = async () => {
-    const { stdout, terminate } = mdfind({
-      query: buildQuery(),
-      // @ts-ignore
-      directories: macosAppPaths,
-    })
-    const iconDir = path.join(getApplicationSupportPath(), 'launcher-icons')
-    if(!await fs.promises.access(iconDir).catch(err => false)) {
-      await fs.promises.mkdir(iconDir, { recursive: true })
-    }
-    let list: any = await stdout
-    list = await Promise.all(list.map(async (app: any) => {
-      const iconPath = path.join(iconDir, app.name + '.png')
-      await fileIcon.file(app.path, {
-        destination: iconPath,
-        size: 64,
-      })
-      return {
-        ...app,
-        icon: 'localfile://' + iconPath,
-      }
-    }))
-    this.apps = list.map((app: App) => ({
-      code: app.name,
-      subtitle: app.path,
-      title: app.name,
-      icon: app.icon,
-      path: app.path,
-      key: app.path,
-      onEnter: (app: App) => {
-        const { exec } = require('child_process')
-        exec(`open -a "${app.path}"`)
-      }
-    }))
+    this.apps = <AppListItem[]>(await getAppList())
   }
 
   onInput(
@@ -107,6 +61,11 @@ class LauncherPlugin implements PublicPlugin {
       return item.code?.toLocaleLowerCase().includes(keyword)
         || pinyinMatch(item.title, keyword)
     }))
+  }
+
+  onEnter (app: AppListItem) {
+    const { exec } = require('child_process')
+    exec(`open -a "${app.path}"`)
   }
 }
 
