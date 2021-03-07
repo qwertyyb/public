5e3380a4f08c3de1cbd906ebb51f7a2fb161eab3
qwertyyb <qwertyyb@qq.com>
2021-03-07 22:17:56 +0800

    feat: 添加必应搜索

--- render/src/utils/autoAdjustWindowSize.ts
+++ render/src/utils/autoAdjustWindowSize.ts
@@ -1,6 +1,8 @@
 const resizeObserver = new window.ResizeObserver((entries: any) => {
   const { width, height } = entries.pop().contentRect;
-  window.ipcRenderer.invoke('ResizeWindow', { width, height })
+  setTimeout(() => {
+    window.ipcRenderer.invoke('ResizeWindow', { width, height })
+  }, 200)
 })
 
 const controller = {
